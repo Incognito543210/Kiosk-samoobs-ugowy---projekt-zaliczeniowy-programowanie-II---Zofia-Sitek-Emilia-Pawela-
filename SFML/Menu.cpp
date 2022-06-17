ba70a5afd65add5d1542667d3b37d709ab7e35ab
Emilia Pawela <ep303242@student.polsl.pl>
2022-06-17 19:59:48 +0200

    Zmiana grafik i tesktu, dodanie ostrzeżenia o ilosci produktów w kreatorach oraz dodtanie informacji że aby zapłaicć musi być chodź jedna rzecz w koszyku

--- SFML/Menu.cpp
+++ SFML/Menu.cpp
@@ -11,27 +11,27 @@ Menu::Menu(float width, float height)
 
 	menu[0].setFont(font);
 	menu[0].setFillColor(Color::Red);
-	menu[0].setPosition(64.f,245.f);
+	menu[0].setPosition(60.f,245.f);
 
 	menu[1].setFont(font);
 	menu[1].setFillColor(Color::Black);
-	menu[1].setPosition(64.f, 340.f);
+	menu[1].setPosition(60.f, 340.f);
 
 	menu[2].setFont(font);
 	menu[2].setFillColor(Color::Black);
-	menu[2].setPosition(64.f, 423.f);
+	menu[2].setPosition(60.f, 423.f);
 
 	menu[3].setFont(font);
 	menu[3].setFillColor(Color::Black);
-	menu[3].setPosition(64.f, 510.f);
+	menu[3].setPosition(60.f, 510.f);
 
 	menu[4].setFont(font);
 	menu[4].setFillColor(Color::Black);
-	menu[4].setPosition(64.f, 595.f);
+	menu[4].setPosition(60.f, 595.f);
 
 	menu[5].setFont(font);
 	menu[5].setFillColor(Color::Black);
-	menu[5].setPosition(530.f,762.f);
+	menu[5].setPosition(525.f, 761.f);
 
 
 
@@ -41,7 +41,7 @@ Menu::Menu(float width, float height)
 Menu::~Menu()
 {};
 
-void Menu::nazwa(string naz, int k)
+void Menu::name(string naz, int k)
 {
 	menu[k].setString(naz);
 
